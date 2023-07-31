d63147fb91aa7b553a61bde13ce8a2b1642e1e45
Prashant Khanchandani <92585387+Piyush-K108@users.noreply.github.com>
2023-07-31 16:35:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,44 +7,45 @@ from kivymd.uix.list import MDList,OneLineAvatarListItem,ImageLeftWidget
 import time
 
 
-Window.size = (350,900)
+
 
 Builder.load_file("homescreen.kv")
 class Homescreen(MDScreen):
-	def __init__(self,**kwargs):
-		super().__init__(**kwargs)
+	pass
+	# def __init__(self,**kwargs):
+	# 	super().__init__(**kwargs)
 
-		# GET SELECTOR FROM KV FILE CAMERA 
-		self.mycamera = self.ids.camera
-		self.myimage = Image()
-		self.resultbox = self.ids.resultbox
-		self.mybox = self.ids.mybox
+	# 	# GET SELECTOR FROM KV FILE CAMERA 
+	# 	self.mycamera = self.ids.camera
+	# 	self.myimage = Image()
+	# 	self.resultbox = self.ids.resultbox
+	# 	self.mybox = self.ids.mybox
 
 
 
-	def captureyouface(self):
-		# CREATE TIMESTAMP NOT FOR YOU FILE IMAGE
-		# THIS SCRIPT GET TIME MINUTES AND DAY NOW
-		timenow = time.strftime("%Y%m%d_%H%M%S")
+	# def captureyouface(self):
+	# 	# CREATE TIMESTAMP NOT FOR YOU FILE IMAGE
+	# 	# THIS SCRIPT GET TIME MINUTES AND DAY NOW
+	# 	timenow = time.strftime("%Y%m%d_%H%M%S")
 
-		# AND EXPORT YOU CAMERA CAPTURE TO PNG IMAGE
-		self.mycamera.export_to_png("myimage_{}.png".format(timenow))
-		self.myimage.source = "myimage_{}.png".format(timenow)
-		self.resultbox.add_widget(
-			OneLineAvatarListItem(
-				ImageLeftWidget(
-					source="myimage_{}.png".format(timenow),
-					size_hint_x=0.3,
-					size_hint_y=1,
+	# 	# AND EXPORT YOU CAMERA CAPTURE TO PNG IMAGE
+	# 	self.mycamera.export_to_png("myimage_{}.png".format(timenow))
+	# 	self.myimage.source = "myimage_{}.png".format(timenow)
+	# 	self.resultbox.add_widget(
+	# 		OneLineAvatarListItem(
+	# 			ImageLeftWidget(
+	# 				source="myimage_{}.png".format(timenow),
+	# 				size_hint_x=0.3,
+	# 				size_hint_y=1,
 
-					# AND SET YOU WIDHT AND HEIGT YOU PHOTO
-					size=(300,300)
+	# 				# AND SET YOU WIDHT AND HEIGT YOU PHOTO
+	# 				size=(300,300)
 
-					),
-				text=self.ids.name.text
-				)
+	# 				),
+	# 			text=self.ids.name.text
+	# 			)
 
-			)
+	# 		)
 
 
 class MyApp(MDApp):
